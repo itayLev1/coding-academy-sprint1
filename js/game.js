c13a074fe643eb51410d3e5aedd9a04fc2e93589
itayLev1 <45080757+itayLev1@users.noreply.github.com>
2021-03-26 13:05:14 +0300

    Add files via upload
    added the abillity to see the number under a tile pressed by the user

--- js/game.js
+++ js/game.js
@@ -3,53 +3,105 @@
 var MINE = '☢️'
 var TILE = '⬜'
 
+// var gGame = {
+//     isOn: false,
+//     shownCount: 0,
+//     markedCount: 0,
+//     secsPassed: 0
+// }
+// var gLevel = {
+//     SIZE: 4,
+//     MINES: 2
+// }
 
-var gBoard;
+var gBoard
+initGame()
+function initGame() {
 
-init()
-function init() {
-    buildMat()
-    renderBoard()
+    gBoard = buildBoard()
+    console.log(gBoard);
+
+    renderBoard(gBoard)
 }
 
+
 // builds the initial matrix and inserts to gBoard
-function buildMat() {
+function buildBoard() {
     var SIZE = 4
     var board = []
     for (var i = 0; i < SIZE; i++) {
         board.push([]);
         for (var j = 0; j < SIZE; j++) {
-            var location = { i, j }
-            board[i][j] = { id: { i, j }, minesAroundCount: 0, isShown: true, isMine: false, isMarked: false }
+            board[i][j] = { minesAroundCount: 0, isShown: false, isMine: false, isMarked: false }
         }
     }
     board[1][3].isMine = true
     board[2][3].isMine = true
-    gBoard = board
-    console.log(gBoard);
+    setMinesNegsCount(board)
+    return board
 }
 
+
 // renders the game board according to gBoard
-function renderBoard() {
-    // debugger 
+function renderBoard(board) {
     var strHTML = '<table class="table">\n<tbody>\n';
-    for (var i = 0; i < gBoard.length; i++) {
+    for (var i = 0; i < board.length; i++) {
         strHTML += '<tr>\n';
-        for (var j = 0; j < gBoard[0].length; j++) {
-            var cell = gBoard[i][j]
-            if (cell.isMine) { cell = MINE }
-            if (cell.minesAroundCount) { cell = cell.minesAroundCount }
-
-
-            strHTML += `<td id="${i}-${j}" onclick="cellClick(this)">${cell}</td>\n`
-
+        for (var j = 0; j < board[0].length; j++) {
+            // debugger
+            if (!board[i][j].isShown) {
+                strHTML += `<td id="${i},${j}" onclick="cellClicked(this)">${TILE}</td>\n`
+            } else {
+                if (board[i][j].isMine) {
+                    strHTML += `<td id="${i},${j}" onclick="cellClicked(this)">${MINE}</td>\n`
+                }
+                if (board[i][j].minesAroundCount > 0) {
+                    strHTML += `<td id="${i},${j}" onclick="cellClicked(this)">${board[i][j].minesAroundCount}</td>\n`
+                }
+            }
         }
         strHTML += '</tr>\n'
     }
     strHTML += '</tbody>\n</table>';
-    // console.log(strHTML);
     var elGameContainer = document.querySelector('.game-container');
     elGameContainer.innerHTML = strHTML;
 }
+
+function cellClicked(elCell) {
+    var nextBoard = gBoard
+    nextBoard[elCell.id[0]][elCell.id[2]].isShown = true
+    console.log(nextBoard)
+    gBoard = nextBoard
+    renderBoard(gBoard)
+}
+
+
+function callMarked() {
+
+}
+
+function checkGameOver() {
+
+}
+
+function expandShown(board, elCell, i, j) {
+
+}
+
+
+
+
+
+    // console.log(elCell);
+    // var elCellInnerText = elCell.innerText
+    // if (Number.isInteger(elCellInnerText)) {
+    //     setMinesNegsCount(gBoard)
+    //     // console.log('after if' ,elCellInnerText);
+    //     gBoard[elCell.id[0]][elCell.id[1]].isShown = true
+    //     console.log('gBoard after click' ,gBoard);
+    // }
+    // console.log(gBoard);
+    // renderBoard()
+
 
 
---
+++ js/util.js
@@ -0,0 +1,29 @@
+'use strict'
+
+
+
+function setMinesNegsCount(gBoard) {
+    var numsStore = []
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            if (!gBoard[i][j].isMine) {
+                gBoard[i][j].minesAroundCount = findMinesNegs(gBoard, i, j);
+                numsStore += gBoard[i][j].minesAroundCount
+            }
+        }
+    }
+}
+
+
+function findMinesNegs(board, i, j) {
+    var res = 0
+    for (var idx = i - 1; idx <= i + 1; idx++) {
+        for (var jdx = j - 1; jdx <= j + 1; jdx++) {
+            if (jdx === -1 || idx === -1 || jdx === board.length || idx === board.length) continue
+            if (board[idx][jdx].isMine) {
+                res++
+            }
+        }
+    }
+    return res;
+}
